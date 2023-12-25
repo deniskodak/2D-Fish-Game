08cc2f17524ffb42f05782dc8430688ab36ab5ff
Denys Kodak <dkodak@deloitte.ua>
2023-12-25 11:08:52 +0200

    add base structure

---
+++ src/index.ts
@@ -0,0 +1,49 @@
+import Game from "./game/Game";
+import "./index.css";
+
+window.addEventListener("load", () => {
+  const startGameButton = document.getElementById("start-game");
+  const welcomeDialog = document.getElementById(
+    "welcome-dialog",
+  ) as HTMLDialogElement;
+  // root canvas element
+  const canvas = document.getElementById("canvas1") as HTMLCanvasElement;
+  // root canvas context
+  const canvasContext = canvas.getContext("2d") as CanvasRenderingContext2D;
+  // game board width
+  canvas.width = 1000;
+  // game board height
+  canvas.height = 500;
+
+  startGameButton.addEventListener("click", () => {
+    welcomeDialog.open = false;
+    canvas.style.opacity = '1';
+
+    const game = new Game(canvas.width, canvas.height);
+
+    // stores last animation timestamp
+    let lastTimeStamp = 0;
+
+    /**
+     * Trigger infinite animation loop
+     */
+    function animation(DOMHighResTimeStamp: number) {
+      // difference between pred and cur animation iteration in ms
+      const deltaTimeStamp = DOMHighResTimeStamp - lastTimeStamp;
+      // update last time stamp for next loop
+      lastTimeStamp = DOMHighResTimeStamp;
+      // remove previous rendered canvas element
+      canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+      // trigger rendering all elements
+      game.draw(canvasContext);
+      // trigger re-calculate all elements positions
+      game.update(deltaTimeStamp);
+
+      // calls our function on each repaint stage
+      requestAnimationFrame(animation);
+    }
+
+    // starts infinity animation loop with zero time stamp
+    animation(0);
+  });
+});
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require("path");
+const CleanWebpackPlugin = require("clean-webpack-plugin").CleanWebpackPlugin;
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.ts",
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {},
+          },
+          "css-loader",
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "**/*",
+          context: path.resolve(__dirname, "assets"),
+          to: "./assets",
+        },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+      },
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
